178f44e0e26befe68e772ab864425bfd0fa6bb6a
Jordi Farré <jordifr@gmail.com>
2017-10-12 19:17:14 +0200

    extracted max width and height constants

--- scala/src/main/scala/Rover.scala
+++ scala/src/main/scala/Rover.scala
@@ -9,6 +9,10 @@ class Rover {
 
   var directions = List('N', 'E', 'S', 'W')
 
+  val MAX_WIDTH = 10
+
+  val MAX_HEIGHT = 10
+
   def execute(commands: String): String =
     commands
       .toCharArray
@@ -39,13 +43,13 @@ class Rover {
 
   def move(command: Char) = {
     if ((command == 'F' && direction == 'E') || (command == 'B' && direction == 'W'))
-      x = (x + 1) % 10
+      x = (x + 1) % MAX_WIDTH
     if ((command == 'F' && direction == 'S') || (command == 'B' && direction == 'N'))
-      y = (y + 1) % 10
+      y = (y + 1) % MAX_HEIGHT
     if ((command == 'F' && direction == 'W') || (command == 'B' && direction == 'E'))
-      if (x == 0) x = 10 - 1 else x -= 1
+      if (x == 0) x = MAX_WIDTH - 1 else x -= 1
     if ((command == 'F' && direction == 'N') || (command == 'B' && direction == 'S'))
-      if (y == 0) y = 10 - 1 else y -= 1
+      if (y == 0) y = MAX_HEIGHT - 1 else y -= 1
   }
 
 }
